9dd7b14128e72fcc98e200d033cd410993efad18
xynico <jiangxiaowei981109@gmail.com>
2022-08-08 10:41:26 -0400

    TODO:::dd

--- src/direct_regression.py
+++ src/direct_regression.py
@@ -9,7 +9,7 @@ from utils import *
 
 def direct_regression_survey_EEG(folder_name,dataset,config):
     if not os.path.exists(os.path.join(config['SAVE_PATH'],folder_name)): os.makedirs(os.path.join(config['SAVE_PATH'],folder_name))
-    EEG_feature = {}
+    EEG_feature = {key:[] for key in config['EEG_FEATURE']}
     for term in dataset.eeg_data.keys():
         for event in dataset.eeg_data[term].keys():
             for subj_id in dataset.eeg_data[term][event].keys():
